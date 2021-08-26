269bd868ba410ed2ba91942ef2e9fbcd85f2e6dd
Daniel Fernandes <daniel_frodrigues@hotmail.com>
2021-08-25 22:10:06 -0300

    Realizado a comunicação com do Wifi e API em looping.

--- main/main.ino
+++ main/main.ino
@@ -1,89 +1,114 @@
 #include <LWiFi.h>
 #include <LWiFiClient.h>
+#include <LTask.h>
 
-#define WIFI_AP "Nome de seu WIF"
-#define WIFI_PASSWORD "Senha do seu wifi"
-#define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
+#define WIFI_AP "nome_de_seu_wifi"
+#define WIFI_PASSWORD "senha_do_seu_wifi"
+#define WIFI_AUTH LWIFI_WPA // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
 
-LWiFiClient client;
-IPAddress server(999, 999, 9, 99); // Ipv4 da API
-
-String latitude = "2327.4283";
-String longitude = "04637.2179";
 int id = 1;
-boolean disconnectedMsg = false;
+LWiFiClient client;
 
 void setup()
 {
-  Serial.println("Inicio");
   LWiFi.begin();
   Serial.begin(115200);
-
-  // keep retrying until connected to AP
-  Serial.println("Connecting to AP");
-  while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
-  {
-    delay(1000);
-  }
-
-  // keep retrying until connected to website
-  Serial.println("Connecting to WebSite");
-  while (0 == client.connect(server, 5000))
-  {
-    Serial.println("Re-Connecting to WebSite");
-    delay(1000);
-  }
-
-  // send HTTP request, ends with 2 CR/LF
-  String PostData = montaBody();
-  Serial.println("Connected. Sending HTTP GET Request ...."); 
-  client.println("POST /Insert HTTP/1.1");  //modify the POST path
-  client.println("Host: Localhost");
-  client.println("Connection: close");
-  client.print("Content-Length: ");
-  client.println(PostData.length());
-  client.println();
-  client.println(PostData);  //send the HTTP POST body
-
-  // waiting for server response
-  Serial.println("waiting HTTP response:");
-  while (!client.available())
-  {
-    delay(100);
-  }
+  connectWifi("setup");
 }
 
 void loop()
 {
-  // Make sure we are connected, and dump the response content to Serial
-  while (client)
+  while (1)
   {
-    int v =client.read();
-    if (v != -1)
+    connectAPI();
+
+    connectWifi("loop");
+
+    // send HTTP request, ends with 2 CR/LF
+    String PostData = montaBody("9999.4283", "99999.2179");
+    Serial.println("[API] Connectado. Enviando HTTP Post Request ....");
+    client.println("POST /Insert HTTP/1.1"); //modify the POST path
+    client.println("Host: Localhost");
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(PostData.length());
+    client.println();
+    client.println(PostData); //send the HTTP POST body
+
+    // waiting for server response
+    Serial.println("[API] Aguardando Retorno...");
+    while (!client.available())
     {
-      Serial.print((char)v);
+      delay(100);
     }
-    else
+
+    int value;
+    while (client.available())
     {
-      Serial.println("no more content, disconnect");
-     client.stop();
-      while (1)
+      value = client.read();
+      if (value < 0)
       {
-        delay(1);
+        Serial.println("[API] Não houve nenhum retorno da API.");
+        break;
       }
+      Serial.print((char)value); // Add this line to the code in test case
     }
+    Serial.println("[API] Finalizado");
+
+    delay(1000);
+  }
+}
+
+void connectAPI()
+{
+  // keep retrying until connected to website
+  Serial.println("[API] Conectando");
+  IPAddress server(999, 999, 9, 99); // Ipv4 da API
+  while (0 == client.connect(server, 5000))
+  {
+    Serial.println("[API] Re-Conectando");
+    delay(100);
   }
 
-  if (!disconnectedMsg)
+  Serial.println("[API] Conectado");
+}
+
+void connectWifi(String caminho)
+{
+  LWifiStatus ws = LWiFi.status();
+  boolean status = wifi_status(ws);
+  if (!status)
   {
-    Serial.println("disconnected by server");
-    disconnectedMsg = true;
+    Serial.println("[WIFI-" + caminho + "] Conectando");
+    while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
+    {
+      Serial.println("[WIFI-" + caminho + "] Re-Conectando");
+      delay(500);
+    }
   }
-  delay(500);
+  Serial.println("[WIFI-" + caminho + "] Conectado");
+}
+
+boolean wifi_status(LWifiStatus ws)
+{
+  switch (ws)
+  {
+  case LWIFI_STATUS_DISABLED:
+    return false;
+    break;
+  case LWIFI_STATUS_DISCONNECTED:
+    return false;
+    break;
+  case LWIFI_STATUS_CONNECTED:
+    return true;
+    break;
+  }
+  return false;
 }
 
 //build JSON method
-String montaBody() {
+String montaBody(String latitude, String longitude)
+{
   String data = "{";
   data += "\n";
   data += "\"latitude\": \"";
